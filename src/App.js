25cd17061f3acc203581323868a8cf1efbff4a37
natalia-bay <nat.bayduraeva@gmail.com>
2021-05-25 11:14:22 +0800

    Implemented search form and basic functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/natalia-bay/dictionary-app"
+              target="_blank"
+            >
+              Open-source code
+            </a>{" "}
+            by{" "}
+            <a
+              href="https://www.linkedin.com/in/natalia-bayduraeva-metaxa/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              natalia
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
